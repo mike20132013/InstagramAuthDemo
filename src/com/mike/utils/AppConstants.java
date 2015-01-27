95f5d947e96fc639365d6727d9b33a1c845c2863
Michael Henry Tirkey <michael.tirkey2012@yahoo.com>
2015-01-27 12:48:19 -0500

    Update on App constants
    Added files for making application constants.

---
+++ src/com/mike/utils/AppConstants.java
@@ -0,0 +1,10 @@
+package com.mike.utils;
+
+public class AppConstants {
+
+	public static final String AUTHURL = "https://api.instagram.com/oauth/authorize/";
+    public static final String TOKENURL = "https://api.instagram.com/oauth/access_token";
+    public static final String APIURL = "https://api.instagram.com/v1";
+    public static String CALLBACKURL = "http://www.view-unlimited.com";
+	
+}
